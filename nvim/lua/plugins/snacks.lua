293897db73f18e6df158902fc5bf4a68ac13a2a8
IMythx <88852348+IMythx@users.noreply.github.com>
2024-11-09 23:06:02 +0200

    feat(snacks terminal): terminal position float instead of bottom and rounded borders

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    terminal = {
+      win = {
+        position = "float",
+        border = "rounded",
+      },
+    },
+  },
+}

8c04364b19805191120451fac1ba24c3664eb71c
IMythx <88852348+IMythx@users.noreply.github.com>
2024-12-07 18:56:09 +0200

    alternative live-server

--- nvim/lua/plugins/live-server-nvim.lua
+++ nvim/lua/plugins/live-server-nvim.lua
@@ -1,15 +1,21 @@
 return {
+  -- {
+  --   "ngtuonghy/live-server-nvim",
+  --   event = "VeryLazy",
+  --   build = ":LiveServerInstall",
+  --   opts = {
+  --     custom = {
+  --       "--port=5500",
+  --       "--no-css-inject",
+  --     },
+  --     serverPath = vim.fn.stdpath("data") .. "/live-server/", --default
+  --     open = "folder", -- folder|cwd     --default
+  --   },
+  -- },
   {
-    "ngtuonghy/live-server-nvim",
-    event = "VeryLazy",
-    build = ":LiveServerInstall",
-    opts = {
-      custom = {
-        "--port=5500",
-        "--no-css-inject",
-      },
-      serverPath = vim.fn.stdpath("data") .. "/live-server/", --default
-      open = "folder", -- folder|cwd     --default
-    },
+    "barrett-ruth/live-server.nvim",
+    build = "sudo npm i -g live-server",
+    cmd = { "LiveServerStart", "LiveServerStop" },
+    config = true,
   },
 }
